b7177e9e76bd5f986dc8d27d04955dc0ed3ef8c4
Kartikeya Mishra <Kartikeya9127@outlook.com>
2026-07-14 22:57:36 +0530

    Got the first page going

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const answersContainer = document.getElementById("answers-container");
 const progressBar = document.getElementById("progress");
 const resultScreen = document.getElementById("result-screen");
 const finalScoreSpan = document.getElementById("final-score");
-const totalScoreSpan= document.getElementById("total-score");
+const maxScoreSpan= document.getElementById("max-score");
 const resultMessage = document.getElementById("result-message");
 const restartButton = document.getElementById("restart-btn");
 const startButton = document.getElementById("start-btn");
@@ -56,9 +56,69 @@ const quizQuestions = [
         ],
     },
     {
-        question: "Which of these is NOT a programming language?"
-    }
+        question: "Which of these is NOT a programming language?",
+        answers: [  
+            {text: "Python", correct:false},
+            {text: "Java", correct:false},
+            {text: "Banana", correct:true},
+            {text: "C++", correct:false},
+        ],
+
+    },
+    {
+        question: "What is the chemical symbol for gold?",
+        answers: [
+            {text: "Go", correct:false},
+            {text: "Au", correct:true},
+            {text: "Ag", correct:false},
+            {text: "Gd", correct:false},
+        ],
+    },
+
 
 
 ]
+
+
+//Game State//
+
+let currentQuestionIndex = 0;
+let score = 0;
+let answersDisabled = false;
+
+
+totalQuestionSpan.textContent = quizQuestions.length;
+maxScoreSpan.textContent = quizQuestions.length;
+
+//event  listeners
+
+startButton.addEventListener("click", startQuiz)
+restartButton.addEventListener("click", restartQuiz)
+
+
+function startQuiz(){
+    //reset vars
+    currentQuestionIndex = 0;
+    
+
+    
+    startScreen.classList.remove("active");
+    quizScreen.classList.add("active");
+
+    showQuestion()
+
+    
+
+}
+
+function showQuestion() {
+
+
+
+}
+
+function restartQuiz(){
+    console.log("quiz re-started")
+}
+
 
